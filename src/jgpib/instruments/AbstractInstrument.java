551d487f58bf210472d95e1982f9f1ee99e715fd
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-11-02 10:04:26 -0500

    abstract instument added.

--- src/jgpib/instruments/AbstractInstrument.java
+++ src/jgpib/instruments/AbstractInstrument.java
@@ -1,9 +1,30 @@
 package jgpib.instruments;
 
+import jgpib.jvisa.JVisaInstrument;
+
 public class AbstractInstrument {
 	
 	/*
 	 * setting up the general GPIB commands here
 	 */
+	
+	JVisaInstrument instrument = new JVisaInstrument() ;
 
+	String fullAddress ;
+	int address ;
+	String name ;
+	
+	public int getAddress() {
+		return address ;
+	}
+	
+	public String getfullAddress() {
+		return fullAddress ;
+	}
+	
+	public String identify() {
+		return null ;
+	}
+	
+	
 }
